7648838c30df4fd98a8f76a991baed21d33e4746
jiayac <jiayanch@umich.edu>
2023-09-22 16:31:58 -0700

    first commit

--- RazorPagesWithRazorClassLibraryAndEsproj/RazorClassLib/ExampleJsInterop.cs
+++ RazorPagesWithRazorClassLibraryAndEsproj/RazorClassLib/ExampleJsInterop.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.Hosting;
 using Microsoft.JSInterop;
 
 namespace RazorClassLib;
@@ -12,10 +13,17 @@ public class ExampleJsInterop : IAsyncDisposable
 {
     private readonly Lazy<Task<IJSObjectReference>> moduleTask;
 
-    public ExampleJsInterop(IJSRuntime jsRuntime)
+    public ExampleJsInterop(IJSRuntime jsRuntime, IHostEnvironment hostEnvironment)
     {
+        if (hostEnvironment.IsDevelopment())
+        {
+            moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
+            "import", "./_Content/RazorClassLib/app.js").AsTask());
+        }
+        else {
         moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>(
-            "import", "./_content/RazorClassLib/exampleJsInterop.js").AsTask());
+            "import", "./_Content/RazorClassLib/app.min.js").AsTask());
+        }
     }
 
     public async ValueTask<string> Prompt(string message)
